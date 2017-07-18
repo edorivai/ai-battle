62ed1c6ccc7f0f31d24b34fe73087bf328f0fa2f
Michael Köppl <mko.mko31@gmail.com>
2017-07-18 21:12:12 +0200

    Add calmandniceperson player

--- src/players/players.js
+++ src/players/players.js
@@ -3,11 +3,13 @@ import PassivePlayer from './passivePlayer';
 import TurtlePlayer from './turtlePlayer';
 import ThiefPlayer from './thiefPlayer';
 import EdoPlayer from './edoPlayer';
+import CalmAndNicePersonPlayer from './calmandnicepersonPlayer';
 
 export default [
 	RandomPlayer,
 	PassivePlayer,
 	TurtlePlayer,
 	ThiefPlayer,
-	EdoPlayer
+	EdoPlayer,
+	CalmAndNicePersonPlayer
 ];
